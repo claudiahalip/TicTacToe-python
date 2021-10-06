e06ea3f43ee239806422064141e266b62abb4538
claudiahalip <claudia4ro@yahoo.com>
2021-10-06 16:55:21 -0500

    testing pre-push hook

--- tests/play_game_test.py
+++ tests/play_game_test.py
@@ -4,8 +4,7 @@ class TestPlayGame:
     
     def test_start(self):
         assert True == True
-    def test_start_2(self):
-        assert 1 == 2
+   
     def test_start_3(self):
         assert 1 == 6
    
