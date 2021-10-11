616dd4a26991555e1cb84a178eac0baa88f871cd
claudiahalip <claudia4ro@yahoo.com>
2021-10-11 09:46:22 -0500

    git pre-push test

--- tests/play_game_test.py
+++ tests/play_game_test.py
@@ -4,5 +4,6 @@ from ..lib.play_game import *
 class TestPlayGame:
     def test_start(self):
         assert True == True
-    def test_start_1(self):
-        assert True == False
+
+    def test_start(self):
+        assert 1== 1
